dc89ce7b6dfe8071a2952be70916c001334040ab
Oliver Jakoubek <mail@oliverjakoubek.de>
2023-02-05 12:07:45 +0100

    Added Artisan commands for dates endpoints

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+use App\Services\DatesService;
+
 /*
 |--------------------------------------------------------------------------
 | Console Routes
@@ -13,6 +15,35 @@ use Illuminate\Support\Facades\Artisan;
 | simple approach to interacting with each command's IO methods.
 |
 */
+
+Artisan::command('dates:today', function () {
+   $this->info((new DatesService())->today());
+});
+Artisan::command('dates:tomorrow', function () {
+    $this->info((new DatesService())->tomorrow());
+});
+Artisan::command('dates:yesterday', function () {
+    $this->info((new DatesService())->yesterday());
+});
+Artisan::command('dates:this-year', function () {
+    $this->info((new DatesService())->thisYear());
+});
+Artisan::command('dates:next-year', function () {
+    $this->info((new DatesService())->nextYear());
+});
+Artisan::command('dates:last-year', function () {
+    $this->info((new DatesService())->lastYear());
+});
+Artisan::command('dates:this-month', function () {
+    $this->info((new DatesService())->thisMonth());
+});
+Artisan::command('dates:next-month', function () {
+    $this->info((new DatesService())->nextMonth());
+});
+Artisan::command('dates:last-month', function () {
+    $this->info((new DatesService())->lastMonth());
+});
+
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
